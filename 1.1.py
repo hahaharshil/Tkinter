c63f29939be276956dfb6b96bce502930c28fba6
Harshil kumar <harshil@harshil-System>
2020-10-07 08:44:36 +0530

    Initial commit

---
+++ 1.1.py
@@ -0,0 +1,27 @@
+from  tkinter import *
+import os
+
+root = Tk()
+
+canvas = Canvas(root, height = 700 , width = 700 , bg = "#131418" )
+canvas.pack()
+
+fra = Frame(root, bg = "#c40e61")
+fra.place()
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
